e1b249b0843b229f6c8c2821d8470e5abccb75be
Otavio Carneiro <git@carneiro.blog.br>
2019-11-20 02:31:08 -0300

    Ponggit status

---
+++ pong-local/sketch.js
@@ -0,0 +1,150 @@
+let webcamvideo;
+let canvas;
+let model;
+const tamanho = {x: 640, y: 480}
+let maoEsquerda = {x: tamanho.x/2, y: tamanho.y/2};
+let maoDireita = {x: tamanho.x/2, y: tamanho.y/2};
+let bola = {x: tamanho.x/2, y: 0};
+
+let velY = 3;
+let velX = 3;
+
+let confianca = 0;
+
+
+// carrega o modelo PoseNet
+async function loadMyModel() {
+	
+    const config = {
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      inputResolution: 417, //  { width: 640, height: 480 },
+      multiplier: 0.75,
+      modelUrl: '/models/075/model-stride16.json'
+    };
+    
+	const net = await posenet.load(config); 
+    return net;
+}
+
+// estima pose a partir de um modelo e uma imagem
+async function estimate(net, imageElement) {
+
+	const imageScaleFactor = 0.5;
+  const outputStride = 16;
+  const flipHorizontal = false;
+	
+  const pose = await net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride);
+  // console.log(pose);
+
+  return pose;
+}
+
+// função executada pelo p5.js inicialmente
+async function setup() {
+  frameRate(10);
+  createCanvas(tamanho.x, tamanho.y);
+  canvas = document.getElementsByClassName("p5Canvas")[0];
+  canvas.width = tamanho.x;
+  canvas.height = tamanho.y;
+  webcamvideo = createCapture(VIDEO);
+  webcamvideo.hide();
+  
+  model = await loadMyModel();
+  await estimate(model, canvas);
+}
+
+// pose = objeto do PoseNet
+// chave = número do keypoint (ex: 0 = nariz)
+// ponto = array de duas posições (x e y) a ser alterado
+// suavizacao = grau de lerping
+function mudaPosicao(pose, chave, ponto, suavizacao) {
+    let x = pose.keypoints[chave].position.x;
+    let y = pose.keypoints[chave].position.y;
+    console.log(x, y);
+    ponto.x = lerp(ponto.x, x, suavizacao);
+    ponto.y = lerp(ponto.y, y, suavizacao);
+    return ponto;
+}
+
+function atualizaPosicoes(pose) {
+  confianca = pose.score;
+  // console.log(pose);
+  if (pose.score > 0.3) {
+    maoEsquerda = mudaPosicao(pose, 9, maoEsquerda, 1.0);
+    maoDireita = mudaPosicao(pose, 10, maoDireita, 1.0);
+  }
+}
+
+// função executada pelo p5.js a cada frame
+async function draw() {
+
+  // mostra vídeo espelhado horizontalmente
+  //    no tamanho do canvas
+  scale(0.5);
+
+  scale(-1, 1);
+  image(webcamvideo, -tamanho.x, 0);
+  scale(-1, 1);
+  
+  estimate(model, canvas).then(atualizaPosicoes);
+
+  fill(255, 255, 255);
+  textSize(32);
+  text(confianca, 5, tamanho.y - 10);
+
+  // desenha bola verde
+  stroke(255, 255, 255); 
+  fill(0, 255, 0);
+  ellipse(bola.x, bola.y, 20);
+
+  // aplica velocidades à bola
+  bola.y = bola.y + velY;
+  bola.x = bola.x + velX;
+
+  // desenha raquetes
+  fill(0, 0, 255);
+  stroke(255, 255, 255);
+  rect(maoEsquerda.x, maoEsquerda.y, 20, 60, 10);
+  rect(maoDireita.x, maoDireita.y, 20, 60, 10);
+
+  // trata colisões
+  /*
+  if (bola.y > 460  &&
+       bola.x > xMaoDir  &&
+       bola.x < xMaoDir + 60) {
+    velY = -20;
+    if (bola.x > xMaoDir + 30) {
+      velX = 3;
+    } else {
+      velX = -3;
+    }
+  }
+  if (bola.y > 460  &&
+       bola.x > xMaoEsq  &&
+       bola.x < xMaoEsq + 60) {
+    velY = -20;
+    if (bola.x > xMaoEsq + 30) {
+      velX = 3;
+    } else {
+      velX = -3;
+    }
+  }
+  */
+
+  // bola passando limite vertical
+  if (bola.y > tamanho.y) {
+    velY = - velY;
+  } else if (bola.y < 0) {
+    velY = - velY;
+  } 
+
+  // bola passando limite horizontal
+  if (bola.x > tamanho.x) {
+    bola.x = 0;
+  }
+  if (bola.x < 0) {
+    bola.x = tamanho.x;
+  }
+
+}
