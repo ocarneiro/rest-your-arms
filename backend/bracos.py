22e98c28666f60a92f73b1932b1fa6d62789d6c9
jetson <git@carneiro.blog.br>
2021-03-07 17:52:36 -0300

    Informa tempo faltante no log

--- backend/bracos.py
+++ backend/bracos.py
@@ -4,6 +4,9 @@ from bottle import route, run, template
 
 arduino_presente = True
 
+# tempo em segundos permitido entre envio de comandos ao Arduino
+tempo_entre_execucoes = 10
+
 try:
     # Tenta conexão com um Arduino que esteja usando o código StandardFirmata
     board = Arduino('/dev/ttyACM0')   # define porta usada pelo Arduino
@@ -16,20 +19,25 @@ except:
     print("Iniciando sem um arduino")
     arduino_presente = False
 
-timestamp = time.time()
+hora_ultima_execucao = time.time()
 
 @route('/braco/<pos>')
 def index(pos):
     print("Recebi %s" % pos)
-    global timestamp
-    if time.time() - timestamp > 10:
+    global hora_ultima_execucao
+    hora_atual = time.time()
+    if hora_atual - hora_ultima_execucao > tempo_entre_execucoes:
         if arduino_presente:
             servo1.write(int(pos))
         else:
             print("Servo: %s" % pos)
         timestamp = time.time()
     else:
-        print("espere")
+        print("Espere %d segundos" % int(tempo_entre_execucoes
+                                         - (hora_atual
+                                         - hora_ultima_execucao)
+                                        )
+              )
     return template('<b>Hello {{pos}}</b>!', pos=pos)
 
 run(host='0.0.0.0', port=8080)
